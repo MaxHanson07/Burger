41c25c2c4c878e969f309ba756105af495fddc1d
Max Hanson <mthanson2@gmail.com>
2020-10-16 15:52:48 -0700

    Fixed typo

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,9 +18,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "devour"
+    "burger_name", "devoured"
   ], [
-    req.body.name, req.body.devour
+    req.body.burger_name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
